2a191b59a33d5532cb8ea2870aa8f67ef1d1c76d
Student <example@example.com>
2019-03-13 21:00:16 -0700

    add initials

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName ) {
+  this.firstName = firstName;
+  this.lastName =  lastName;
+
+}
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+//user Interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    debugger;
+    event.preventDefault();
+
+    var inputFirstName = $("input#new-first-name").val();
+    var inputLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputFirstName, inputLastName);
+
+$("ul#contactList").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+ $(".contact").last().click(function() {
+   $("#show-contact").show();
+   $("#show-contact h2").text(newContact.firstName);
+   $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
+ });
+ $("input#new-first-name").val("");
+   $("input#new-last-name").val("");
+  });
+});
